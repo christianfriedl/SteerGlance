d9f334f14b8324f5f778e0039af39c80620ed155
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2015-04-09 08:01:12 +0200

    refactor aggregates

--- js/test/server/sql/sqlite/testQuery.js
+++ js/test/server/sql/sqlite/testQuery.js
@@ -6,6 +6,7 @@ var async = require('async');
 var table = require('server/sql/table.js');
 var field = require('server/sql/field.js');
 var condition = require('server/sql/condition.js');
+var aggregate = require('server/sql/aggregate.js');
 var query = require('server/sql/query.js');
 var sqlDb = require('server/sql/db.js');
 var sqliteQuery = require('server/sql/sqlite/query.js');
@@ -86,8 +87,7 @@ function testAggregateQuery() {
     table1.field(id1);
     var id2 = field.field('id2', field.Type.int);
     table2.field(id2);
-    var s = query.select(sumField)
-            .aggregate(query.Aggregate.sum)
+    var s = query.select(aggregate.aggregate(aggregate.Type.sum, sumField))
             .from(table1, table2)
             .where(condition.condition(id1, condition.Op.eq, id2));
     var sqliteQQ = sqliteQuery.query(s);
