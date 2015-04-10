681baf0be267da385143bcce6e3a2dde62a3049b
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2015-04-10 07:55:02 +0200

    sanitize test directories and add new tests

---
+++ js/test/testDao.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var _ = require('underscore');
+var assert = require('assert');
+var async = require('async');
+var table = require('dao/dao.js');
+var table = require('sql/table.js');
+var field = require('sql/field.js');
+var index = require('sql/index.js');
+var condition = require('sql/condition.js');
+var aggregate = require('sql/aggregate.js');
+var query = require('sql/query.js');
+var ddl = require('sql/ddl.js');
+var sqlDb = require('sql/db.js');
+var sqliteQuery = require('sql/sqlite/query.js');
+
+
+
+function runTests() {
+    throw 'none defined yet';
+}
+
+exports.runTests = runTests;
