fbb8175d9463491f6443571774e32d2a1b965a7b
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2015-04-27 16:03:11 +0200

    fix primary dao

--- js/test/app/customer/customerBo.js
+++ js/test/app/customer/customerBo.js
@@ -1,40 +1,41 @@
-var customerBo = require('app/customer/customerBo.js');
-var db = require('sql/db.js');
+var async = require('async');
+var m_app_customer_customerBo = require('app/customer/customerBo.js');
+var m_sql_db = require('sql/db.js');
 var assert = require('assert');
+var m_TestSuite = require('TestSuite.js');
 
-var Tests = {
+var tests = {
+    _name: 'testCustomerBo',
     testUpdate: function() {
-        var db1 = db.db(':memory:').open(':memory:');
-        var bo1 = customerBo.customerBo(db1);
-        bo1.id(1);
-        bo1.firstName('Christian');
-        bo1.lastName('Friedl');
-        bo1.save();
-        /*
-        var bo2 = customerBo.customerBo(db1);
-        bo2.loadById(1, function(err) {
-            if ( err ) throw err;
-            console.log(bo2);
-            assert.strictEqual('Christian', bo2.firstName());
-            assert.strictEqual('Friedl', bo2.lastName());
-        });
+        var db1 = m_sql_db.db(':memory:').open(':memory:');
+        var bo1 = m_app_customer_customerBo.customerBo(db1);
+        async.series([
+            function(callback) { db1.runSql('CREATE TABLE customer (id int, firstName text, lastName text)', [], callback); },
+            function(callback) { db1.runSql('INSERT INTO customer (id, firstName, lastName) VALUES(1, \'Hargenbrihl\', \'Zackenbruck\')', [], callback); },
+            function(callback) {
+                bo1.id(1);
+                bo1.firstName('Christian');
+                bo1.lastName('Friedl');
+                bo1.save();
+                /*
+                var bo2 = customerBo.customerBo(db1);
+                bo2.loadById(1, function(err) {
+                    if ( err ) throw err;
+                    console.log(bo2);
+                    assert.strictEqual('Christian', bo2.firstName());
+                    assert.strictEqual('Friedl', bo2.lastName());
+                });
 
-        */
-
+                */
+            }],
+            function(err, result) { if ( err ) throw err; console.log(result); }
+        );
     }
 };
 
 function runTests() {
-    console.log('>', module.filename);
-    var f = null;
-    for (f in Tests) {
-        if ( typeof(Tests[f]) === 'function' && f.substr(0,4) === 'test' ) {
-            console.log('>>>', f);
-            Tests[f]();
-            console.log('<<<', f);
-        }
-    }
-    console.log('<', module.filename);
+    m_TestSuite.TestSuite.call(tests);
+    m_TestSuite.TestSuite.prototype.runTests.call(tests);
 }
 
 exports.runTests = runTests;
--- js/test/dao/testDao.js
+++ js/test/dao/testDao.js
@@ -13,9 +13,10 @@ var aggregate = require('sql/aggregate.js');
 var query = require('sql/query.js');
 var ddl = require('sql/ddl.js');
 var sqliteQuery = require('sql/sqlite/query.js');
+var m_dao_primaryDao = require('dao/primaryDao.js');
 
 var Tests = {
-    testFields: function() {
+    ItestFields: function() {
         var id1 = field.field('id1', field.Type.int).value(1);
         var name1 = field.field('name1', field.Type.string).value('name');
         var table1 = table.table().field(id1).field(name1);
@@ -23,7 +24,7 @@ var Tests = {
         assert.strictEqual(id1, dao1.field('id1'));
         assert.strictEqual(name1, dao1.field('name1'));
     },
-    testGetters: function() {
+    ItestGetters: function() {
         var id1 = field.field('id1', field.Type.int).value(1);
         var name1 = field.field('name1', field.Type.string).value('one');
         var table1 = table.table().field(id1).field(name1);
@@ -33,7 +34,7 @@ var Tests = {
         assert.strictEqual('one', dao1.name1());
     },
 
-    testSetters: function() {
+    ItestSetters: function() {
         var id1 = field.field('id1');
         assert.strictEqual('id1', id1.accessorName());
         var name1 = field.field('name1');
@@ -46,7 +47,7 @@ var Tests = {
         assert.strictEqual('name', dao1.name1());
     },
 
-    testLoadByQuery: function() {
+    ItestLoadByQuery: function() {
         var table1 = table.table('table1');
         var id1 = field.field('id1', field.Type.int);
         table1.field(id1);
@@ -57,8 +58,8 @@ var Tests = {
         var select = query.select(id1).from(table1).where(cond);
         var db1 = db.db(':memory:').open(':memory:');
         async.series([
-            function(callback) { db1._db.runSql('CREATE TABLE table1 (id1 int)', []); },
-            function(callback) { db1._db.runSql('INSERT INTO table1 (id1) VALUES(1)', []); },
+            function(callback) { db1._db.runSql('CREATE TABLE table1 (id1 int)', [], callback); },
+            function(callback) { db1._db.runSql('INSERT INTO table1 (id1) VALUES(1)', [], callback); },
             function(callback) {
                 var dao1 = dao.dao(db1, table1);
                 dao1.loadByQuery(select, function(err) {
@@ -73,21 +74,24 @@ var Tests = {
 
     testPrimaryDao: function() {
         var table1 = table.table('table1')
-            .field(field.field('id', field.Type.int))
-            .field(field.field('name', field.Type.string));
+                        .name('table1')
+                        .field(field.field('id', field.Type.int))
+                        .field(field.field('name', field.Type.string));
         var db1 = db.db(':memory:').open(':memory:');
         async.series([
-            function(callback) { db1._db.runSql('CREATE TABLE table1 (id int, name text)', []); },
-            function(callback) { db1._db.runSql('INSERT INTO table1 (id, name) VALUES(1, \'full name\')', []); },
+            function(callback) { db1._db.runSql('CREATE TABLE table1 (id int, name text)', [], callback); },
+            function(callback) { db1._db.runSql('INSERT INTO table1 (id, name) VALUES(1, \'full name\')', [], callback); },
             function(callback) {
-                var dao1 = dao.dao(db1, table1);
+                var dao1 = m_dao_primaryDao.primaryDao(db1, table1);
                 dao1.loadById(1, function(err) {
-                    if ( err ) throw err;
+                    if ( err ) callback(err);
                     console.log('dao laoded', dao1.id());
                     assert.strictEqual(1, dao1.id());
                     assert.strictEqual('full name', dao1.name());
+                    callback();
                 });
-            }],
+            }
+            ],
             function(err, result) { if ( err ) throw err; console.log(result); }
         );
     }
