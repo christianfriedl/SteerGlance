75caabe9cb6241a4d4376a25e7cc3b3ea2a0bd8a
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2015-05-02 16:59:33 +0200

    use sane logger (log4js), thanx boni

--- js/test/runTests.js
+++ js/test/runTests.js
@@ -1,3 +1,6 @@
+var log4js = require('log4js');
+log4js.configure({ appenders: [ { type: "console", layout: { type: "basic" } } ], replaceConsole: true })
+
 // framework tests
 
 require('./sql/testField.js').runTests();
