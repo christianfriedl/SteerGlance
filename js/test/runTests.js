373d27ff2bdc143d432b4104f0e18516ffb19f10
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2015-05-06 08:12:56 +0200

    factor-out writable fields list; add clone for calcfield; fix bug causing update to try to write calcfields to db

--- js/test/runTests.js
+++ js/test/runTests.js
@@ -5,11 +5,11 @@ global.DEBUG = true;
 
 // framework tests
 
-require('./sql/testField.js').runTests();
-require('./sql/sqlite/testQuery.js').runTests();
+//require('./sql/testField.js').runTests();
+//require('./sql/sqlite/testQuery.js').runTests();
 // // require('./server/sql/testQuery.js').runTests(); // this one is currently not active, it's all in sqlite/testquery
-require('./dao/testDao.js').runTests();
-require('./bo/testBo.js').runTests();
+//require('./dao/testDao.js').runTests();
+//require('./bo/testBo.js').runTests();
 require('./app/customer/customerBo.js').runTests();
 
 // server tests
