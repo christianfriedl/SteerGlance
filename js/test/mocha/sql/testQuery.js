558531cf663135aff7e716e8f56fd3f592ed80b6
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2016-10-19 20:50:31 +0200

    sql/query should have real getters/setters; rename files for better CamelCasing

--- js/test/mocha/sql/testQuery.js
+++ js/test/mocha/sql/testQuery.js
@@ -5,19 +5,9 @@ var model_EntityModel = require('model/EntityModel.js');
 
 describe('model_EntityModel', function() {
     describe('create', function() {
-        it('should return an object with getter and setter delegates', function() {
-            const object = { 'abc': 'ABC', 'def': 'DEF' };
-            const model = model_EntityModel.create(object);
-            assert.strictEqual('ABC', model.getAbc());
-            assert.strictEqual('DEF', model.getDef());
-
-            model.setAbc('xyz');
-            assert.strictEqual('xyz', model.getAbc());
-
-            assert.strictEqual('xyz', model.xyz());
-
+        it('should create a basic query', function() {
             var table1 = sql_Table.create('table1');
-            var field1 = sql_Field.create('field1', field.DataType.int);
+            var field1 = sql_Field.create('field1', sql_Field.DataType.int);
             table1.addField(field1);
             /*
             var cond = filter.filter: function()
@@ -26,6 +16,8 @@ describe('model_EntityModel', function() {
                 .compareTo('haha');
                 */
             var select = sql_Query.select(field1).from(table1);
+
+            assert.strictEqual(1, select.getFields().length);
             // var sqliteQQ = sqlite_Query.create(select);
         });
     });
