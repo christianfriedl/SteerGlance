f6d46c27e610422849040bb1877054df42183fea
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2016-10-19 14:47:06 +0200

    use saner naming conventions

--- js/test/mocha/model/testModel.js
+++ js/test/mocha/model/testModel.js
@@ -1,11 +1,11 @@
 var assert = require('assert');
-var model_Model = require('model/model.js');
+var model_Model = require('model/Model.js');
 
 describe('model_Model', function() {
     describe('createModel', function() {
         it('should return an object with getters and setters', function() {
             const object = { 'abc': 'ABC', 'def': 'DEF' };
-            const model = model_Model.createModel(object);
+            const model = model_Model.create(object);
             assert.strictEqual('ABC', model.getAbc());
             assert.strictEqual('DEF', model.getDef());
 
