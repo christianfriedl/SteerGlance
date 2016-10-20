986ae452d8164953089c653452a4add4ef2c02d0
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2016-10-20 18:25:44 +0200

    query should not overwrite its fields upon setting table

--- js/test/mocha/sql/sqlite/testQuery.js
+++ js/test/mocha/sql/sqlite/testQuery.js
@@ -3,6 +3,7 @@
 var assert = require('assert');
 var sql_Table = require('sql/Table.js');
 var sql_Field = require('sql/Field.js');
+var sql_ValueField = require('sql/ValueField.js');
 var sql_Query = require('sql/Query.js');
 var sql_Filter = require('sql/Filter.js');
 var sql_sqlite_Query = require('sql/sqlite/Query.js');
@@ -22,21 +23,19 @@ describe('sql_sqlite_Query', function() {
         it('should output a sqlite update query', function() {
             var table1 = sql_Table.create('table1');
             var field1 = sql_Field.create('field1', sql_Field.DataType.int);
-            field1.setValue(1);
             table1.addField(field1);
             var cond = sql_Filter.create(field1, sql_Filter.Op.eq, 'haha');
-            var select = sql_Query.update(field1).from(table1).where(cond);
+            var update = sql_Query.update(sql_ValueField.create(field1, 1)).from(table1).where(cond);
 
-            var sqliteQQ = sql_sqlite_Query.create(select);
+            var sqliteQQ = sql_sqlite_Query.create(update);
             assert.strictEqual('UPDATE table1 SET field1 = ? WHERE table1.field1 = ?', sqliteQQ.getQueryString());
             assert.deepStrictEqual([1, 'haha'], sqliteQQ.getParams());
         });
         it('should output a sqlite insert query', function() {
             var table1 = sql_Table.create('table1');
             var field1 = sql_Field.create('field1', sql_Field.DataType.int);
-            field1.setValue(1);
             table1.addField(field1);
-            var insert = sql_Query.insert(field1).into(table1);
+            var insert = sql_Query.insert(sql_ValueField.create(field1, 1)).into(table1);
 
             var sqliteQQ = sql_sqlite_Query.create(insert);
             assert.strictEqual('INSERT INTO table1 (field1) VALUES (?)', sqliteQQ.getQueryString());
