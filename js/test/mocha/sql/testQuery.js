c3e1bab04a7efbf04b7e60c208d284870c6347b4
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2016-10-20 14:54:22 +0200

    be able to create a simple select query

--- js/test/mocha/sql/testQuery.js
+++ js/test/mocha/sql/testQuery.js
@@ -8,7 +8,7 @@ var sql_Filter = require('sql/Filter.js');
 
 describe('model_EntityModel', function() {
     describe('create', function() {
-        it('should create a basic query', function() {
+        it('should create a select query', function() {
             var table1 = sql_Table.create('table1');
             var field1 = sql_Field.create('field1', sql_Field.DataType.int);
             table1.addField(field1);
@@ -23,6 +23,12 @@ describe('model_EntityModel', function() {
             assert.strictEqual('table1', select.getTables()[0].getName());
             console.log(select);
             // var sqliteQQ = sqlite_Query.create(select);
+        });
+        it('should create a update query', function() {
+            assert.ok(false, 'test is not implemented');
+        });
+        it('should create a insert query', function() {
+            assert.ok(false, 'test is not implemented');
         });
     });
 });
