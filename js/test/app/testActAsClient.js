8de0fe449fea6452eb4846cb64fcbd1851e3e8c4
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2016-11-24 12:42:39 +0100

    be able to serve one customer edit screen by new promises method

--- js/test/app/testActAsClient.js
+++ js/test/app/testActAsClient.js
@@ -19,16 +19,21 @@
 
 /////////////////////////////////// largely TODO, this is only partly moved from old testing infrastructure
 
-var _ = require('underscore');
+var _ = require('lodash');
 var async = require('async');
 var sql_DB = require('sql/DB.js');
 var assert = require('assert');
-var server_DefaultController = require('server/DefaultController.js');
 var util = require('util');
+var server_DefaultController = require('server/DefaultController.js');
+var app_customer_CustomerEntitySet = require('app/customer/CustomerEntitySet.js');
+var app_customer_CustomerEntity = require('app/customer/CustomerEntity.js');
 
-function setupDb(db1, callback) {
-    async.series([
-        function(callback) { db1.runSql('CREATE TABLE customer (id int, firstName text, lastName text)', [], callback); },
+function setupDb(db1) {
+    return db1.runSql('CREATE TABLE customer (id int, name text)', []).then( () => {
+        return db1.runSql('INSERT INTO customer (id, name) VALUES(1, \'Christian\')', []);
+    });
+        /*
+
         function(callback) { db1.runSql('INSERT INTO customer (id, firstName, lastName) VALUES(1, \'Christian\', \'Friedl\')', [], callback); },
         function(callback) { db1.runSql('INSERT INTO customer (id, firstName, lastName) VALUES(2, \'Hargenbrihl\', \'Zackenbruck\')', [], callback); },
         function(callback) { db1.runSql('CREATE TABLE invoice (id int, amount int, customerId int)', [], callback); },
@@ -55,13 +60,52 @@ function setupDb(db1, callback) {
             );
         }
     ], callback);
+        */
 }
 
-xdescribe('server', function() {
-    it('should fetch customer list', function() { // can test calcfield...
+describe('server', function() {
+    let db1;
+    beforeEach(function(done) {
+        db1 = sql_DB.create(':memory:').open(':memory:');
+        setupDb(db1).then(() => { done(); });
+    });
+    afterEach(function() {
+        db1.close();
+    });
+    it('should fetch customer edit screen', function(done) {
+        var entitySet = app_customer_CustomerEntitySet.create(db1);
+        var request = {
+            query: {
+                id: 1
+            }
+        };
+        var response = {};
+        server_DefaultController.edit(entitySet, request, response).then((response) => {
+            console.log('response', response);
+            /*
+            console.log('testFetchCustomerList: "responsecallback" received response', response, 'with rows', util.inspect(response.data.rows, { depth: 3} ));
+            console.log('testFetchCustomerList: "responsecallback" with templateRow', util.inspect(response.data.templateRow, { depth: 3} ));
+            console.log('testFetchCustomerList: "responsecallback" with aggregateRow', util.inspect(response.data.aggregateRow, { depth: 3} ));
+            assert.strictEqual('list', response.action);
+            assert.strictEqual(2, response.data.count);
+            assert.ok(response.data.rows);
+            assert.strictEqual(2, response.data.rows.length);
+            assert.ok(response.data.aggregateRow);
+            assert.ok(response.data.aggregateRow.fields);
+            assert.ok(response.data.templateRow.fields);
+            assert.ok(response.data.rows[0].fields);
+            */
+
+            done(); 
+        }).catch((e) => {
+            console.error('error in chain', e);
+            done(e);
+        });
+    });
+                        /*
+    it.skip('should fetch customer list', function() { // can test calcfield...
         var db1 = sql_DB.create(':memory:').open(':memory:');
-        var daoSet = m_dao_daoSet.daoSet(db1, m_app_customer_customerDao.customerDao);
-        var boSet = m_bo_boSet.boSet(db1, daoSet, m_app_customer_customerBo.customerBo);
+        var entitySet = app_customer_CustomerEntitySet.create(db1);
         var request = {};
         var response = {};
         async.series([
@@ -79,6 +123,7 @@ xdescribe('server', function() {
                         assert.ok(response.data.aggregateRow.fields);
                         assert.ok(response.data.templateRow.fields);
                         assert.ok(response.data.rows[0].fields);
+                        /*
                         assert.strictEqual(1, response.data.rows[0].id);
                         assert.strictEqual(1, _(response.data.rows[0].fields).findWhere({ name: 'id' }).value);
                         assert.strictEqual(550, _(response.data.rows[0].fields).findWhere({ name: 'sumInvoiceAmount' }).value);
@@ -91,9 +136,9 @@ xdescribe('server', function() {
 
                 }
         ]);
-
     });
-    it('should fetch invoice list', function() {
+                        */
+    it.skip('should fetch invoice list', function() {
         var db1 = sql_DB.create(':memory:').open(':memory:');
         var daoSet = m_dao_daoSet.daoSet(db1, m_app_invoice_invoiceDao.invoiceDao);
         var boSet = m_bo_boSet.boSet(db1, daoSet, m_app_invoice_invoiceBo.invoiceBo);
@@ -111,7 +156,7 @@ xdescribe('server', function() {
                 }
         ]);
     });
-    it('shold fetch invoice list with limit', function() {
+    it.skip('shold fetch invoice list with limit', function() {
         var db1 = sql_DB.create(':memory:').open(':memory:');
         var daoSet = m_dao_daoSet.daoSet(db1, m_app_invoice_invoiceDao.invoiceDao);
         var boSet = m_bo_boSet.boSet(db1, daoSet, m_app_invoice_invoiceBo.invoiceBo);
@@ -128,7 +173,7 @@ xdescribe('server', function() {
                 }
         ]);
     });
-    it('should fetch invoice list with imits and orderby', function() {
+    it.skip('should fetch invoice list with imits and orderby', function() {
         var db1 = sql_DB.create(':memory:').open(':memory:');
         var daoSet = m_dao_daoSet.daoSet(db1, m_app_invoice_invoiceDao.invoiceDao);
         var boSet = m_bo_boSet.boSet(db1, daoSet, m_app_invoice_invoiceBo.invoiceBo);
@@ -163,7 +208,7 @@ xdescribe('server', function() {
         ]);
 
     });
-    it('should fetch invoice list with filter', function() {
+    it.skip('should fetch invoice list with filter', function() {
         var db1 = sql_DB.create(':memory:').open(':memory:');
         var daoSet = m_dao_daoSet.daoSet(db1, m_app_invoice_invoiceDao.invoiceDao);
         var boSet = m_bo_boSet.boSet(db1, daoSet, m_app_invoice_invoiceBo.invoiceBo);
