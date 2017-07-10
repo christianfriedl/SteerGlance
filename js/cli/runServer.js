0d727e50bead0c3f1f0b185744e74bb6d6225803
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2017-07-10 19:33:52 +0200

    add configurability to server

--- js/cli/runServer.js
+++ js/cli/runServer.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const web_server_Server = require('web/server/Server.js');
+const configReader = require('configReader.js');
 
-const config = { web: { server: { port: 8888 } } };
+const config = configReader.readConfig();
 
 web_server_Server.create(config).run();
