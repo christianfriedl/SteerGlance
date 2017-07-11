44adab124f6c454b85ffa4443eabb1b04aa6e4c7
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2017-07-11 11:48:01 +0200

    remove as-for-now unneeded module name from routing

--- js/test/web/server/testRouter.js
+++ js/test/web/server/testRouter.js
@@ -7,30 +7,26 @@ const web_server_router = require('web/server/router.js');
 describe('router', function() {
     it('should route / to index/index', function() {
         const desc = web_server_router.route('/');
-        assert.strictEqual('index', desc.module);
-        assert.strictEqual('index', desc.subModule);
-        assert.strictEqual('index', desc.action);
+        assert.strictEqual('index', desc.controllerName);
+        assert.strictEqual('index', desc.actionName);
     });
     it('should route .js to index/index/serveFile', function() {
         const desc = web_server_router.route('/a/b/c.js');
-        assert.strictEqual('index', desc.module);
-        assert.strictEqual('index', desc.subModule);
-        assert.strictEqual('serveFile', desc.action);
+        assert.strictEqual('index', desc.controllerName);
+        assert.strictEqual('serveFile', desc.actionName);
     });
     it('should route .css to index/index/serveFile', function() {
         const desc = web_server_router.route('/a/b/c.css');
-        assert.strictEqual('index', desc.module);
-        assert.strictEqual('index', desc.subModule);
-        assert.strictEqual('serveFile', desc.action);
+        assert.strictEqual('index', desc.controllerName);
+        assert.strictEqual('serveFile', desc.actionName);
     });
     it('should route a/b/c to correct request desc', function() {
-        const desc = web_server_router.route('/a/b/c');
-        assert.strictEqual('a', desc.module);
-        assert.strictEqual('b', desc.subModule);
-        assert.strictEqual('c', desc.action);
+        const desc = web_server_router.route('/a/b');
+        assert.strictEqual('a', desc.controllerName);
+        assert.strictEqual('b', desc.actionName);
     });
     it('should route a/b/c?x=y to correct request desc', function() {
-        const desc = web_server_router.route('/a/b/c?x=y');
+        const desc = web_server_router.route('/a/b?x=y');
         assert.ok(desc.query);
         assert.ok(desc.query.x);
         assert.strictEqual('y', desc.query.x);
